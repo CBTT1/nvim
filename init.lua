861567a34f4b8b982d9efa8b86068725ae9a496c
CBTT1 <714039364@qq.com>
2023-05-28 20:37:16 +0800

    add conflict-marker

--- init.lua
+++ init.lua
@@ -157,7 +157,19 @@ require("lazy").setup({
 		event = "VeryLazy",
 		"rhysd/conflict-marker.vim",
 		config = function()
-			require("gitsigns").setup()
+			vim.cmd([[
+		let g:conflict_marker_highlight_group = ''
+		
+		" Include text after begin and end markers
+		let g:conflict_marker_begin = '^<<<<<<< .*$'
+		let g:conflict_marker_end   = '^>>>>>>> .*$'
+		
+		highlight ConflictMarkerBegin guibg=#2f7366
+		highlight ConflictMarkerOurs guibg=#2e5049
+		highlight ConflictMarkerTheirs guibg=#344f69
+		highlight ConflictMarkerEnd guibg=#2f628e
+		highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+			]])
 		end,
 	},
 	{
@@ -358,3 +370,17 @@ cmp.setup.cmdline(":", {
 		{ name = "cmdline" },
 	}),
 })
+
+vim.cmd([[
+		let g:conflict_marker_highlight_group = ''
+		
+		" Include text after begin and end markers
+		let g:conflict_marker_begin = '^<<<<<<< .*$'
+		let g:conflict_marker_end   = '^>>>>>>> .*$'
+		
+		highlight ConflictMarkerBegin guibg=#2f7366
+		highlight ConflictMarkerOurs guibg=#2e5049
+		highlight ConflictMarkerTheirs guibg=#344f69
+		highlight ConflictMarkerEnd guibg=#2f628e
+		highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+]])
